ede5236f1be87f2af84db14a11db1f6f9fbf6826
Ahmed2004401 <ahmedhossam#529@gmail.com>
2024-11-23 02:10:41 +0200

    some changes because errors appeared

--- lib/providers/SettingsProvider.dart
+++ lib/providers/SettingsProvider.dart
@@ -6,69 +6,69 @@ import 'package:shared_preferences/shared_preferences.dart';
 // observers
 // listeners
 // subscribers
-class SettingsProvider extends ChangeNotifier{
-
+class SettingsProvider extends ChangeNotifier {
   ThemeMode themeMode = ThemeMode.light;
-SettingsProvider(){
-  gettheme();
-
-}
-  changeTheme(ThemeMode newTheme){
-    if(newTheme == themeMode) return;
+  SettingsProvider() {
+    gettheme();
+    getlan();
+  }
+  changeTheme(ThemeMode newTheme) {
+    if (newTheme == themeMode) return;
     themeMode = newTheme;
+    savetheme(themeMode);
     notifyListeners();
   }
 
   String language = "en";
 
-  changeLanguage(String newLanguage){
-    if(language == newLanguage) return;
+  changeLanguage(String newLanguage) {
+    if (language == newLanguage) return;
     language = newLanguage;
+    savelan(language);
     notifyListeners();
   }
-void savetheme(ThemeMode theme)async{
 
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-if(theme==ThemeMode.light){
-
-  prefs.setString('theme', 'light');
-}else{
-  prefs.setString('theme', 'dark');
-
-}
-
-}
-void gettheme()async{
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String cachedTheme =prefs.getString('theme')??'light';
-  if(cachedTheme=='light'){
-    themeMode=ThemeMode.light;
-  }
-  else{
-    themeMode=ThemeMode.dark;
-  }
-  notifyListeners();
-}
-void savelan(String lan)async{
-
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-    if(lan=='en'){
-prefs.setString(language, 'en');
-
-    }else{
-      prefs.setString(language, 'ar');
-
+  void savetheme(ThemeMode theme) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (theme == ThemeMode.light) {
+      prefs.setString('theme', 'light');
+    } else {
+      prefs.setString('theme', 'dark');
     }
-}
-void getlan()async{
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  String cachedlan =prefs.getString('language')??'en';
-if(cachedlan=='en'){
-language=='en';
-}
-else{
-  language='ar';
-}
-notifyListeners();
-}
+  }
+
+  void gettheme() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String cachedTheme = prefs.getString('theme') ?? 'light';
+    if (cachedTheme == 'light') {
+      themeMode = ThemeMode.light;
+    } else {
+      themeMode = ThemeMode.dark;
+    }
+    notifyListeners();
+  }
+
+  void savelan(String lan) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    if (lan == 'en') {
+      prefs.setString('language', 'en');
+    } else {
+      prefs.setString('language', 'ar');
+    }
+  }
+
+  void getlan() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String lan = prefs.getString('language') ?? 'en';
+
+    if (lan == 'ar') {
+      language = 'ar';
+    } else {
+      language = 'en';
+    }
+
+    notifyListeners();
+  }
 }
